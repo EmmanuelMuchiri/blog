67cd28c3f7b474fe011dcbc3b08f9cd07fc4f7db
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-10 12:05:17 +0300

    created the request.py file to consume data from the stormconsultancy json object

---
+++ app/request.py
@@ -0,0 +1,14 @@
+import urllib.request,json
+
+def get_quote():
+   '''
+   Function that gets the json response to our url request
+   '''
+   get_randomquotes_url = 'http://quotes.stormconsultancy.co.uk/random.json'
+
+   with urllib.request.urlopen(get_randomquotes_url) as url:
+       quotes = url.read()
+       get_sources_response = json.loads(quotes)
+       print(quotes)
+
+   return get_sources_response
