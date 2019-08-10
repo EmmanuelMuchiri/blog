8f17afa9eb0cb721cbf81aa3991a0d49c4224401
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-10 23:23:05 +0300

    adding a blog post working perfectly

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,12 @@
-from flask import render_template, request, redirect, url_for,abort
+from flask import render_template,request,redirect,url_for,abort
 from . import main
-from .. import db,photos
+from .forms import PostForm,SubscriberForm,CommentForm
+from ..import db,photos
+from ..models import User,Post,Role,Subscriber,Comment
 from flask_login import login_required,current_user
+import markdown2
+from ..email import mail_message
 from ..request import get_quote
-from app.models import User
 
 @main.route('/')
 def index():
@@ -22,3 +25,25 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+@main.route("/new_post",methods=['GET','POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        new_post=Post(title=title,post=post,category=category)
+
+        new_post.save_post()
+
+        subscribers=Subscriber.query.all()
+
+        for subscriber in subscribers:
+            mail_message("New Blog Post","email/new_post",subscriber.email,post=new_post)
+
+        return redirect(url_for('main.index'))
+
+    title="Make a post"
+    return render_template('new_post.html',title=title,post_form=form)
