a52722400e3ce936c33b30de8c05c01c573e6700
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-10 22:10:18 +0300

    added table blogs and comments and linked them

--- app/models.py
+++ app/models.py
@@ -40,3 +40,55 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+    
+    
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String())
+    blog_content = db.Column(db.String())
+    posted = db.Column(db.DateTime, nullable=False, default = datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    @classmethod
+    def get_all_blogs(cls):
+        blogs = Blog.query.order_by('id').all()
+        return blogs
+    @classmethod
+    def get_single_blog(cls,id):
+        blog = Blog.query.filter_by(id=id).first()
+        return blog
+
+    
+
+
+class Comment(db.Model):
+    
+    __tablename__='comments'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    email = db.Column(db.String())
+    comment_content = db.Column(db.String())
+    date_comment = db.Column(db.DateTime, nullable=False, default = datetime.utcnow)
+    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    @classmethod
+    def get_blog_comments(cls,id):
+        comments = Comment.query.filter_by(blog_id=id).order_by('id').all()
+        return comments
+    
+    @classmethod
+    def get_single_comment(cls,id_blog,id):
+        comment = Comment.query.filter_by(blog_id=id_blog,id=id).first()
+
+        return comment   
