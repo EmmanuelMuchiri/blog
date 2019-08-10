0226b52d12bb2dfeec150c6df999dafbd4f319f7
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-10 14:45:38 +0300

    imported the db

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for,abort
 from . import main
-# from .. import db,photos
+from .. import db,photos
 from flask_login import login_required,current_user
 from ..request import get_quote
 
