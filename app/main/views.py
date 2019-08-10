cb7ef8079a1f185cf068e4e046e4d4e449c7c5e2
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-10 12:13:28 +0300

    created the views.py inside the main directory and define the route that the quotes will be directed to.I also imported the get_quote function from request.py

---
+++ app/main/views.py
@@ -0,0 +1,13 @@
+from flask import render_template, request, redirect, url_for,abort
+from . import main
+from .. import db,photos
+from flask_login import login_required,current_user
+from ..request import get_quote
+
+@main.route('/')
+def index():
+
+    name  = "Quote"
+    quote = get_quote()
+    
+    return render_template('index.html',name = name,quote = quote)
